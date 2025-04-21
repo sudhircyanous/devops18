48cfab83b3089976d2b10911ab3194ad81ff701f
sudhircyanous <56716846+sudhircyanous@users.noreply.github.com>
2025-04-21 12:39:27 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,24 +2,24 @@ resource "aws_s3_bucket" "one" {
   bucket = "sud.devops.project.bucket"
 }
 
-resource "aws_s3_bucket_ownership_controls" "two" {
+resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.one.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "three" {
-  depends_on = [aws_s3_bucket_ownership_controls.two]
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership]
+  bucket     = aws_s3_bucket.one.id
+  acl        = "private"
+}
 
+resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.one.id
-  acl    = "private"
-}
 
-resource "aws_s3_bucket_versioning" "three" {
-bucket = aws_s3_bucket.one.id
-versioning_configuration {
-status = "Enabled"
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
-}
-
